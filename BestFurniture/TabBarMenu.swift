5eab021f394d43210ad4219e427b9eb7e5bc62f0
Valery <74760519+ihValery@users.noreply.github.com>
2021-07-03 03:18:39 +0300

    Start

--- BestFurniture/TabBarMenu.swift
+++ BestFurniture/TabBarMenu.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TabBarMenu.swift
 //  BestFurniture
 //
 //  Created by Валерий Игнатьев on 3.07.21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TabBarMenu: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TabBarMenu()
     }
 }
