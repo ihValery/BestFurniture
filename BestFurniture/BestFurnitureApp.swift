72e0f7d9f3062eda100bd6e8d91b4549d99f7ae4
Valery <74760519+ihValery@users.noreply.github.com>
2021-07-03 03:07:27 +0300

    Initial Commit

---
+++ BestFurniture/BestFurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFurnitureApp.swift
+//  BestFurniture
+//
+//  Created by Валерий Игнатьев on 3.07.21.
+//
+
+import SwiftUI
+
+@main
+struct BestFurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
